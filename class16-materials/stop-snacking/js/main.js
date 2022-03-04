a70ec1d533ecedf268a0ed4f378da1e9ff5d31e8
Brandon Heine <brandonmheine@gmail.com>
2022-03-03 17:39:20 -0800

    Finished stop-snacking

--- class16-materials/stop-snacking/js/main.js
+++ class16-materials/stop-snacking/js/main.js
@@ -1,1 +1,17 @@
 //Create a function that grabs the number of snacks from the input and tells you to stop that many times
+
+const input = document.querySelector('input');
+const clicker = document.querySelector('#help');
+clicker.addEventListener('click', stops);
+
+const output = document.querySelector('#stops');
+
+
+function stops() {
+    outputText = '';
+    for (let i = 0; i < input.value; i++) {
+        outputText += 'STOP ';
+    }
+    console.log(outputText);
+    output.textContent = outputText;
+}
