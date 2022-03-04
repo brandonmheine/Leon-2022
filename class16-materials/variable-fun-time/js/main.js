0e699ad1cac3416f240be8c06083d48faef6547a
Brandon Heine <brandonmheine@gmail.com>
2022-03-03 17:08:19 -0800

    Added variable fun time

--- class16-materials/variable-fun-time/js/main.js
+++ class16-materials/variable-fun-time/js/main.js
@@ -1,18 +1,31 @@
 //--- Easy
 //create a variable and assign it a number
+let number = 1;
 
 //minus 10 from that number
+number -= 10;
 
 //print that number to the console
+console.log(number);
 
 //--- Medium
 //create a variable that holds a value from the input
+const input = document.querySelector('input');
 
 //add 25 to that number
+// inputValue += 25;
 
 //alert that number
+// alert(inputValue);
 
 //--- Hard
 //create a variable that holds the h1
+const h1 = document.querySelector('h1');
 
 //add an event listener to that element that console logs the sum of the two previous variables
+h1.addEventListener('click', sum)
+
+function sum() {
+    let inputValue = input.value;
+    console.log(number + Number(inputValue));
+}
