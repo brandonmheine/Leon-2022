7867f17a6a014d252cd50406177cffb88254cede
Brandon Heine <brandonmheine@gmail.com>
2022-03-03 17:31:26 -0800

    Finished 21

--- class16-materials/sir-savage-the-21st/js/main.js
+++ class16-materials/sir-savage-the-21st/js/main.js
@@ -1,2 +1,14 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
 //Bonus can you make it print '21' 21 times to the dom?
+
+const say = document.querySelector('h2');
+
+function twentyOne() {
+    let output = ''
+    for (let i = 0; i < 21; i++) {
+        output += '21 '
+    }
+    return output
+}
+
+say.textContent = twentyOne();
