b5902dec1211f7173462c00534bec50724649b7b
Brandon Heine <brandonmheine@gmail.com>
2022-03-22 14:31:21 -0700

    Finished drink API

--- class20-materials /objects-shots/js/main.js
+++ class20-materials /objects-shots/js/main.js
@@ -4,21 +4,19 @@
 const namePlace = document.querySelector('#name')
 const instructionsPlace = document.querySelector('#instructions')
 const imagePlace = document.querySelector('img') 
-imagePlace.setAttribute('height', '200px')
+imagePlace.setAttribute('height', '300px')
+const ingredientAndAmountPlace = document.querySelector('#ingredients-amounts')
 
 const getCocktail = async function() {
     const searchWord = document.querySelector('input').value
     const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchWord}`)
-    // console.log(response);
     const resJSON = await response.json()
-    // console.log(resJSON)
     const drinks = resJSON.drinks
-    const random = Math.floor(Math.random()* drinks.length)
-    const drink = drinks[random]
-    // console.log(firstDrink.strDrink)
-    // namePlace.textContent = `Drink name: ${drink.strDrink}`
-    // instructions.textContent = `Instructions: ${drink.strInstructions}`
-    // image.src = drink.strDrinkThumb
+    const randomIndex = getRandomIndex(drinks)
+    const drink = drinks[randomIndex]
+    const ingredients = getIngredients(drink)
+    const amounts = getAmounts(drink)
+    displayIngredientsAndAmounts(ingredients, amounts)
     setProperties(drink.strDrink, drink.strInstructions, drink.strDrinkThumb)
 }
 
@@ -26,6 +24,41 @@ function setProperties(name, instructions, image) {
     namePlace.textContent = `Drink name: ${name}`
     instructionsPlace.textContent = `Instructions: ${instructions}`
     imagePlace.src = image
+}
+
+function getRandomIndex(array) {
+    return Math.floor(Math.random()* array.length)
+}
+
+function getIngredients(drink) {
+    const ingredients = Object.entries(drink)
+    const filteredIngredients = ingredients.filter(a => a[0].includes('strIngredient') && a[1] !== null && a[1] !== '')
+    // console.log(filteredIngredients)
+    const ingredientsOnly = filteredIngredients.map(a => a[1])
+    console.log(ingredientsOnly);
+    return ingredientsOnly
+}
+
+function getAmounts(drink) {
+    const amounts = Object.entries(drink)
+    // console.log(amounts);
+    const filteredAmounts = amounts.filter(a => a[0].includes('strMeasure') && a[1] !== null && a[1] !== '')
+    // console.log(filteredAmounts);
+    const amountsOnly = filteredAmounts.map(a => a[1])
+    console.log(amountsOnly);
+    return amountsOnly
+}
+
+function displayIngredientsAndAmounts(ingredients, amounts) {
+    ingredientAndAmountPlace.innerHTML = ''
+    for (let i = 0; i < ingredients.length; i++) {
+        if (amounts[i] !== undefined) {
+            let current = `${ingredients[i]}: ${amounts[i]}`
+            let newDiv = document.createElement('div')
+            newDiv.textContent = current
+            ingredientAndAmountPlace.appendChild(newDiv)
+        }
+    }
 }
 
 // getCocktail()
