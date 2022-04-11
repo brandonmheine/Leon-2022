6e45c025266a24624ea1f706c4f241af9420911a
Brandon Heine <brandonmheine@gmail.com>
2022-04-11 09:39:38 -0700

    Added cocktails

--- class26-materials/objects-dog-photos/js/main.js
+++ class26-materials/objects-dog-photos/js/main.js
@@ -1,1 +1,11 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+
+const imagePlace = document.querySelector('img')
+
+async function getDogPhoto() {
+  const response = await fetch(`https://dog.ceo/api/breeds/image/random`)
+  const responseJSON = await response.json()
+  imagePlace.src = responseJSON.message
+}
+
+getDogPhoto()
--- class26-materials/objects-shots/js/main.js
+++ class26-materials/objects-shots/js/main.js
@@ -1,1 +1,68 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+// www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita
+
+const namePlace = document.querySelector('#name')
+const instructionsPlace = document.querySelector('#instructions')
+const imagePlace = document.querySelector('img') 
+imagePlace.setAttribute('height', '300px')
+const ingredientAndAmountPlace = document.querySelector('#ingredients-amounts')
+
+const getCocktail = async function() {
+    const searchWord = document.querySelector('input').value
+    const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchWord}`)
+    const resJSON = await response.json()
+    const drinks = resJSON.drinks
+    // console.log(drinks);
+    const randomIndex = getRandomIndex(drinks)
+    const drink = drinks[randomIndex]
+    const ingredients = getIngredients(drink)
+    const amounts = getAmounts(drink)
+    displayIngredientsAndAmounts(ingredients, amounts)
+    setProperties(drink.strDrink, drink.strInstructions, drink.strDrinkThumb)
+}
+
+function setProperties(name, instructions, image) {
+    namePlace.textContent = `Drink name: ${name}`
+    instructionsPlace.textContent = `Instructions: ${instructions}`
+    imagePlace.src = image
+}
+
+function getRandomIndex(array) {
+    return Math.floor(Math.random()* array.length)
+}
+
+function getIngredients(drink) {
+    const ingredients = Object.entries(drink)
+    const filteredIngredients = ingredients.filter(a => a[0].includes('strIngredient') && a[1] !== null && a[1] !== '')
+    // console.log(filteredIngredients)
+    const ingredientsOnly = filteredIngredients.map(a => a[1])
+    // console.log(ingredientsOnly);
+    return ingredientsOnly
+}
+
+function getAmounts(drink) {
+    const amounts = Object.entries(drink)
+    // console.log(amounts);
+    const filteredAmounts = amounts.filter(a => a[0].includes('strMeasure') && a[1] !== null && a[1] !== '')
+    // console.log(filteredAmounts);
+    const amountsOnly = filteredAmounts.map(a => a[1])
+    // console.log(amountsOnly);
+    return amountsOnly
+}
+
+function displayIngredientsAndAmounts(ingredients, amounts) {
+    ingredientAndAmountPlace.innerHTML = ''
+    for (let i = 0; i < ingredients.length; i++) {
+        if (amounts[i] !== undefined) {
+            let current = `${ingredients[i]}: ${amounts[i]}`
+            let newDiv = document.createElement('div')
+            newDiv.textContent = current
+            ingredientAndAmountPlace.appendChild(newDiv)
+        }
+    }
+}
+
+// getCocktail()
+
+const button = document.querySelector('button')
+button.addEventListener('click', getCocktail)
