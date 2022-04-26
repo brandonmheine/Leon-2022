cb4d80a43d9298243a859ff3b8e8a8e73f1e275d
Brandon Heine <brandonmheine@gmail.com>
2022-04-26 14:33:19 -0700

    Added class 29 stuff

--- class26-materials/objects-dog-photos/js/main.js
+++ class26-materials/objects-dog-photos/js/main.js
@@ -2,24 +2,24 @@
 
 const imagePlace = document.querySelector('img')
 
-// async function getDisney() {
-//   const response = await fetch(`https://api.disneyapi.dev/characters`)
-//   const responseJSON = await response.json()
-//   const firstImage = responseJSON.data[0].imageUrl
-//   imagePlace.src = firstImage
-// }
+async function getDisney() {
+  const response = await fetch(`https://api.disneyapi.dev/characters`)
+  const responseJSON = await response.json()
+  const firstImage = responseJSON.data[0].imageUrl
+  imagePlace.src = firstImage
+}
 
 getDisney()
 
 
 
-function getDisney() {
-  fetch(`https://api.disneyapi.dev/characters`)
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-  })
-  .catch(err => {
-    console.log(`error: ${err}`)
-  });
-}
+// function getDisney() {
+//   fetch(`https://api.disneyapi.dev/characters`)
+//   .then(res => res.json())
+//   .then(data => {
+//     console.log(data)
+//   })
+//   .catch(err => {
+//     console.log(`error: ${err}`)
+//   });
+// }
